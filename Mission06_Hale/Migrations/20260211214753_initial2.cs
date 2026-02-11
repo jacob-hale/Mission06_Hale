1713092dac52918f1792a6e0abf516f7cf0998fb
Jacob Hale <Jchale23@byui.edu>
2026-02-11 14:51:48 -0700

    got everything working with the post method into the sqlite databse

---
+++ Mission06_Hale/Migrations/20260211214753_initial2.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission06_Hale.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Movies",
+                type: "TEXT",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LentTo",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Edited",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Movies",
+                type: "TEXT",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LentTo",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Edited",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Mission06_Hale/Models/Movie.cs
+++ Mission06_Hale/Models/Movie.cs
@@ -18,10 +18,10 @@ namespace Mission06_Hale.Models
         public string Director { get; set; }
         [Required]
         public string Rating { get; set; }
-        public string Edited { get; set; }
-        public string LentTo { get; set; }
+        public string? Edited { get; set; }
+        public string? LentTo { get; set; }
         [MaxLength(25)]
-        public string Notes { get; set; }
+        public string? Notes { get; set; }
 
 
 
