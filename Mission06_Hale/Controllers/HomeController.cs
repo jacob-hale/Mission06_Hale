7f24b7e4abed934b127d1794891e38235b33d5ac
Jacob Hale <Jchale23@byui.edu>
2026-02-11 14:37:41 -0700

    setting up everything before added the sqlite database

--- Mission06_Hale/Controllers/HomeController.cs
+++ Mission06_Hale/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ namespace Mission06_Hale.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieCollectionContext _context;
+        public HomeController(MovieCollectionContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,10 +20,18 @@ namespace Mission06_Hale.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult AddAMovie()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddAMovie(Movie movie)
+        {
+           _context.Movies.Add(movie);
+           _context.SaveChanges();
+
+            return View("Confirmation", movie);
         }
     }
 }
