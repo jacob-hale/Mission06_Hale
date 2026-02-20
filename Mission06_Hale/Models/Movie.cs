db32478c8a847390d8ed397be8feaafa7ded33b3
Jacob Hale <Jchale23@byui.edu>
2026-02-19 17:12:49 -0700

    fixed some ish

--- Mission06_Hale/Models/Movie.cs
+++ Mission06_Hale/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace Mission06_Hale.Models
         
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
-        public Category Category {  get; set; }
+        public Category? Category {  get; set; }
         [Required]
         public string Title { get; set; }
         
